1d536e628aaf4a00ebc69bd516224a8e046145d3
albertevieites <albertevieites@gmail.com>
2022-08-23 23:55:59 +0100

    trailing

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import { AuthProviderWrapper } from './context/auth.context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    
+
       <AuthProviderWrapper>
         <App />
       </AuthProviderWrapper>
-  
+
   </Router>
 )
