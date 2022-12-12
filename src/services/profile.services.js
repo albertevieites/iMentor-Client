ae3a603a91a0756a08cfed6c8aa890f1dc4ca395
Alberte Vieites <73227233+albertevieites@users.noreply.github.com>
2022-12-12 15:13:00 +0000

    Update profile.services.js

--- src/services/profile.services.js
+++ src/services/profile.services.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class Profile {
   constructor() {
     this.app = axios.create({
-      baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+      baseURL: `${process.env.REACT_APP_API_URL}`,
     });
 
     this.app.interceptors.request.use((config) => {
