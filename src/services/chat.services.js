07553cc2150948320647c9966fd990324456a7c3
Alberte Vieites <73227233+albertevieites@users.noreply.github.com>
2022-12-12 15:12:24 +0000

    Update chat.services.js

--- src/services/chat.services.js
+++ src/services/chat.services.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class chatService {
   constructor() {
     this.api = axios.create({
-      baseURL: process.env.REACT_APP_SERVER_URL,
+      baseURL: process.env.REACT_APP_API_URL,
     });
 
     // Automatically set JWT token in the headers for every request
