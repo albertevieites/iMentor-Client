cc2eb0b9751a555b4ca397564aa30294897a1947
Alberte Vieites <albertevieites@gmail.com>
2022-12-12 10:39:46 +0000

    fix: :bug: Capitalize skills file name

--- src/components/Skills/skills.jsx
+++ src/components/Skills/skills.jsx
@@ -1,59 +1,75 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react";
 
-import skills from "../../services/skills.service"
+import skills from "../../services/skills.service";
 
-import './Skills.css'
+import "./Skills.css";
 
 const Skills = (props) => {
-    const [skillsList, setSkillsList] = useState([]);
+  const [skillsList, setSkillsList] = useState([]);
 
-    useEffect(()=>{
-        skills.get5Skills()
-        .then((skills)=>{
-            setSkillsList (skills.data)})
-        .catch((err)=>console.log(err))
+  useEffect(() => {
+    skills
+      .get5Skills()
+      .then((skills) => {
+        setSkillsList(skills.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    },[])
+  function showAll() {
+    skills
+      .getAllSkills()
+      .then((skills) => {
+        // console.log("skills from card", skills)
+        setSkillsList(skills.data);
+      })
+      .catch((err) => console.log(err));
+  }
 
-    function showAll(){
+  function showLess() {
+    skills
+      .get5Skills()
+      .then((skills) => {
+        // console.log("skills from card", skills)
+        setSkillsList(skills.data);
+      })
+      .catch((err) => console.log(err));
+  }
 
-            skills.getAllSkills()
-            .then((skills)=>{
-                // console.log("skills from card", skills)
-                setSkillsList (skills.data)})
-            .catch((err)=>console.log(err))
+  return (
+    <div className="divChips">
+      {skillsList.map((skill) => {
+        return props.filtering?.includes(skill._id) ? (
+          <span
+            className="chips-selected"
+            key={skill._id}
+            id={skill._id}
+            onClick={(e) => props.function(e)}
+          >
+            {skill.name}
+          </span>
+        ) : (
+          <span
+            className="chips"
+            key={skill._id}
+            id={skill._id}
+            onClick={(e) => props.function(e)}
+          >
+            {skill.name}
+          </span>
+        );
+      })}
+      {skillsList.length === 5 ? (
+        <div onClick={showAll} className="showMore">
+          +
+        </div>
+      ) : (
+        <div onClick={showLess} className="showLess">
+          -
+        </div>
+      )}
+    </div>
+  );
+};
 
-
-    }
-
-    function showLess(){
-
-        skills.get5Skills()
-        .then((skills)=>{
-            // console.log("skills from card", skills)
-            setSkillsList (skills.data)})
-        .catch((err)=>console.log(err))
-
-
-}
-
-    return (
-        <div className="divChips">
-    {skillsList.map((skill)=>{
-        return(
-            props.filtering?.includes(skill._id) ?
-            <span className="chips-selected" key={skill._id} id={skill._id} onClick={(e)=>props.function(e)}>{skill.name}</span> :
-            <span className="chips" key={skill._id} id={skill._id} onClick={(e)=>props.function(e)}>{skill.name}</span>
-            )
-
-    }) }
-        {skillsList.length === 5 ?
-            <div onClick={showAll} className="showMore">+</div>
-            :
-            <div onClick={showLess} className="showLess">-</div>}
-
-    </div>)
-
-}
-
-export default Skills
+export default Skills;
